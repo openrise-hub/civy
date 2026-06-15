6788206ae4b3040b5d55ad310d42e8fca4a6ae4e
Carlos Bolívar <cebemind@hotmail.com>
2026-06-14 23:45:46 -0400

    fix(pdf): replace WOFF2 fonts with WOFF to fix DataView error on PDF download

--- src/lib/fonts/register.ts
+++ src/lib/fonts/register.ts
@@ -3,8 +3,8 @@ import { Font } from "@react-pdf/renderer";
 Font.register({
   family: "Inter",
   fonts: [
-    { src: "/fonts/Inter-Regular.woff2", fontWeight: 400 },
-    { src: "/fonts/Inter-Bold.woff2", fontWeight: 700 },
-    { src: "/fonts/Inter-Italic.woff2", fontWeight: 400, fontStyle: "italic" },
+    { src: "/fonts/Inter-Regular.woff", fontWeight: 400 },
+    { src: "/fonts/Inter-Bold.woff", fontWeight: 700 },
+    { src: "/fonts/Inter-Italic.woff", fontWeight: 400, fontStyle: "italic" },
   ],
 });
